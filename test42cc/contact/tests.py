caf0c176e2d0ef8e92da6b21c7ef4f344c5bf87a
Volodymyr Kovtun <vova-zms@yandex.ua>
2012-04-05 16:38:21 +0300

    ticket:1 fix tests

--- test42cc/contact/tests.py
+++ test42cc/contact/tests.py
@@ -5,36 +5,37 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+#from django.test import TestCase
 from django.utils import unittest
 from django.test.client import Client
 from contact.models import Contact
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
-
 class ContactTest(unittest.TestCase):
-    def setUp(self):
-        self.client = Client()
-
     def test_contact_creation(self):
         """ test for Contact objects creation
         """
         Contact.objects.create(name='name', surname='surname',
             birthday='01.01.1970', email='t@test.local', jabber='jabber@test',
             skype='skype', contacts='contacts')
-        test_contact = Contact.objects.get(name='test_name')
-        self.assertEqual(test_contact.surname, 'test_surname')
+        test_contact = Contact.objects.get(name='name')
+        self.assertEqual(test_contact.surname, 'surname')
         self.assertTrue(Contact.objects.count() > 0)
+
+
+class ViewsTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
 
     def test_index_page(self):
         """ test http code for index page
         """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_fixture_contact(self):
+        """test for data exists loaded with fixtures
+        """
+        search = 'Volodya'
+        response = self.client.get('/')
+        self.assertIn(search, response.content)
