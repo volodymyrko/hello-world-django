d1ada6d563657bbd24cb43b33e8b7b14b9e686a5
Volodymyr Kovtun <vova-zms@yandex.ua>
2012-04-06 14:24:30 +0300

    ticket:3 write tests

--- test42cc/utils42cc/tests.py
+++ test42cc/utils42cc/tests.py
@@ -5,12 +5,35 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+#from django.test import TestCase
+from django.utils import unittest
+from django.test.client import Client
+from utils42cc.models import HttpRequestEntry
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class MiddlewareTest(unittest.TestCase):
+    def setUp(self):
+        """ prepare to testing
         """
-        Tests that 1 + 1 always equals 2.
+        self.client = Client()
+        self.url = '/requests'
+
+    def test_middleware(self):
+        """ middleware testing for saving urls
         """
-        self.assertEqual(1 + 1, 2)
+        self.client.get(self.url)
+        self.assertTrue(HttpRequestEntry.objects.filter(path=self.url))
+
+
+class ShowRequestsTest(unittest.TestCase):
+    def setUp(self):
+        """ prepare to testing
+        """
+        self.client = Client()
+        self.url = '/requests/'
+
+    def test_page_requests(self):
+        """ test page for showing visited urls
+        """
+        page = self.client.get(self.url)
+        self.assertIn(self.url, page.content)
