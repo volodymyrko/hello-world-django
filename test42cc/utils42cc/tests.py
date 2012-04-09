59d6252fb4a50b12251cb66013db61731d198191
Kovtun Volodymyr <vova-zms@yandex.ua>
2012-04-09 18:02:25 +0300

    write test for context processors

--- test42cc/utils42cc/tests.py
+++ test42cc/utils42cc/tests.py
@@ -8,6 +8,9 @@ Replace this with more appropriate tests for your application.
 #from django.test import TestCase
 from django.utils import unittest
 from django.test.client import Client
+from django.template import RequestContext
+from django.http import HttpRequest
+from django.conf import settings
 from utils42cc.models import HttpRequestEntry
 
 
@@ -37,3 +40,15 @@ class ShowRequestsTest(unittest.TestCase):
         """
         page = self.client.get(self.url)
         self.assertIn(self.url, page.content)
+
+
+class ContextProcTest(unittest.TestCase):
+    def setUp(self):
+        self.context = RequestContext(HttpRequest())
+        self.key = 'django_settings'
+        self.processors = settings.TEMPLATE_CONTEXT_PROCESSORS
+        self.proc = 'utils42cc.context_processors.django_settings'
+
+    def test_context_exists(self):
+        self.assertTrue(self.key in self.context)
+        self.assertIn(self.proc, self.processors)
