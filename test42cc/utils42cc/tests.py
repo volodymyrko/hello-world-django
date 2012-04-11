7aa554e7ea9738123878c7d9b5aaa50a6505474b
Kovtun Volodymyr <vova-zms@yandex.ua>
2012-04-11 17:24:56 +0300

    ticket:3 add test for middleware

--- test42cc/utils42cc/tests.py
+++ test42cc/utils42cc/tests.py
@@ -8,6 +8,7 @@ Replace this with more appropriate tests for your application.
 #from django.test import TestCase
 from django.utils import unittest
 from django.test.client import Client
+from django.core.urlresolvers import reverse
 from utils42cc.models import HttpRequestEntry
 
 
@@ -30,10 +31,32 @@ class ShowRequestsTest(unittest.TestCase):
         """ prepare to testing
         """
         self.client = Client()
-        self.url = '/requests/'
+        self.url = reverse('requests')
 
     def test_page_requests(self):
         """ test page for showing visited urls
         """
         page = self.client.get(self.url)
         self.assertIn(self.url, page.content)
+
+    def test_requests(self):
+        """ test requests with middleware
+        """
+        for entry in HttpRequestEntry.objects.all():
+            entry.delete()  # clear http request log table
+
+        num = 15
+        request_per_page = 10
+        t_url = '/request_%s/'
+        t_record = '<td>/request_%s/</td>'
+        for i in range(num):
+            url = t_url % i
+            self.client.get(url)
+
+        page = self.client.get(self.url)
+        for i in range(num):
+            record = t_record % i
+            if i < request_per_page:
+                self.assertIn(record, page.content)
+            else:
+                self.assertNotIn(record, page.content)
