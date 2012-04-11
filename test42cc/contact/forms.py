0bc2e9251d447ff422c34d32b1ae4621b92db0f4
Kovtun Volodymyr <vova-zms@yandex.ua>
2012-04-11 12:21:21 +0300

    ticket:5 add widget for photo and some other fields

--- test42cc/contact/forms.py
+++ test42cc/contact/forms.py
@@ -1,5 +1,27 @@
 from django.forms import ModelForm
+from django.conf import settings
+from django.forms.widgets import Textarea, ClearableFileInput
+from django.utils.safestring import mark_safe
 from contact.models import Contact
+
+
+class BrTextarea(Textarea):
+    """ widget that show label_tag and field value in different rows
+    """
+    def render(self, name, value, attrs=None):
+        result = super(BrTextarea, self).render(name, value, attrs=None)
+        return mark_safe(u'<br />%s' % result)
+
+
+class PhotoWidget(ClearableFileInput):
+    """ widget that show image (<img> tag) with form
+    """
+    def render(self, name, value, attrs=None):
+        result = super(PhotoWidget, self).render(name, value, attrs=None)
+        if value and hasattr(value, 'url'):
+            result = mark_safe(u'%s<br /><img src="%s/%s">' % (result,
+                settings.STATIC_URL, value.url))
+        return result
 
 
 class ContactForm(ModelForm):
@@ -10,3 +32,8 @@ class ContactForm(ModelForm):
         fields = ('name', 'surname', 'birthday',
             'photo', 'email', 'jabber', 'skype',
             'contacts', 'bio')
+        widgets = {
+            'bio': BrTextarea(),
+            'contacts': BrTextarea(),
+            'photo': PhotoWidget(),
+        }
