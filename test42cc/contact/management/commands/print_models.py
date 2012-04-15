4cbcc962a727b113b4983e34bc9ba59d09583108
Kovtun Volodymyr <vova-zms@yandex.ua>
2012-04-15 19:53:00 +0300

    ticket:9 fir command output; add stderr

--- test42cc/contact/management/commands/print_models.py
+++ test42cc/contact/management/commands/print_models.py
@@ -6,4 +6,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for obj in ContentType.objects.all():
             count = obj.get_all_objects_for_this_type().count()
-            print "%s: %s" % (obj.model, count)
+            self.stdout.write("%s: %s\n" % (obj.model, count))
+            self.stderr.write("error: %s: %s\n" % (obj.model, count))
