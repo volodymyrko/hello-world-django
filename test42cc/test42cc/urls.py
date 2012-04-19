8a0c91e23437739a48ff6370b417f80f3bf9fdef
Volodymyr Kovtun <vova-zms@yandex.ua>
2012-04-19 16:54:38 +0300

    ticket:13 change template and add view for enable priority field in request model

--- test42cc/test42cc/urls.py
+++ test42cc/test42cc/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^requests', 'utils42cc.views.requests', name='requests'),
+    url(r'^requests/$', 'utils42cc.views.requests', name='requests'),
+    url(r'^requests/prio/$', 'utils42cc.views.prio', name='prio'),
     url(r'', include('contact.urls')),
 )
--- test42cc/utils42cc/views.py
+++ test42cc/utils42cc/views.py
@@ -2,6 +2,9 @@
 from django.shortcuts import render_to_response
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.template import RequestContext
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.utils import simplejson
 from utils42cc.models import HttpRequestEntry
 
 RECORDS_PER_PAGE = 10
@@ -21,3 +24,30 @@ def requests(request):
         requests = paginator.page(1)
     return render_to_response('requests.html', {'requests': requests},
         context_instance=RequestContext(request))
+
+
+@login_required
+def prio(request):
+    """ change priority
+    """
+    good_actios = ['plus', 'minus']
+    result = {'status': "BAD"}
+    r_id = request.GET.get('id', '')
+    action = request.GET.get('action', '')
+
+    if action not in good_actios:
+        return HttpResponse(simplejson.dumps(result))
+
+    try:
+        obj = HttpRequestEntry.objects.get(id=r_id)
+    except:
+        return HttpResponse(simplejson.dumps(result))
+
+    if action == 'plus':
+        obj.priority += 1
+    elif action == 'minus':
+        obj.priority -= 1
+    obj.save()
+    result = {'status': "OK", 'prio': obj.priority}
+
+    return HttpResponse(simplejson.dumps(result))
